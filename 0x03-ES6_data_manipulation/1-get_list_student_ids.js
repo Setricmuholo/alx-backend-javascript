f645b54d755be764ebd81673322de5ae6a65e316
Setricmuholo <muholosetric@gmail.com>
2023-07-03 22:41:56 +0300

    task completed

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,11 @@
+/*
+ * a function that returns a array of ids from a list of objects
+ */
+
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  return students.map(student => student.id);
+}
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,1 @@
+in this folder i learnt how to use Typed arrays, How to use map, filter and reduce on arrays, the set, map and weak link data structures
